cf65c3bd09d93af18a589425f62a1a2fe61853db
Diogo Duran <duran@sapo.pt>
2022-04-12 10:19:54 +0100

    Fix the pg_dumpall command to prevent dumping priviledges

--- script/postgres-backup.sh
+++ script/postgres-backup.sh
@@ -4,7 +4,7 @@
 cd /home/root
 date1=$(date +%Y%m%d-%H%M)
 mkdir pg-backup
-PGPASSWORD="$PG_PASS" pg_dumpall -h postgresql-postgresql.devtroncd -p 5432 -U postgres -U postgres > pg-backup/postgres-db.tar
+PGPASSWORD="$PG_PASS" pg_dumpall -O -x -h postgresql-postgresql.devtroncd -p 5432 -U postgres -U postgres > pg-backup/postgres-db.tar
 file_name="pg-backup-"$date1".tar.gz"
 
 #Compressing backup file for upload
